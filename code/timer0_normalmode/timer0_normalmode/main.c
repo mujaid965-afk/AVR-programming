7ad774e2e7b3bfdafdd5d832d485971a32955c61
MUHAMMED MUJAID K <mujaid965@gmail.com>
2026-02-12 12:51:34 +0530

    corrected errors

--- code/timer0_normalmode/timer0_normalmode/main.c
+++ code/timer0_normalmode/timer0_normalmode/main.c
@@ -11,29 +11,25 @@
 void delay_1ms()
 {
 
-	for(long int i=0;i<62500;i++) //repeating the process 62500 times to get 1s  delay
+	for(uint32_t i = 0; i < 62500; i++) //repeating the process 62500 times to get 1s  delay
 	{
-	 while((!TIFR0&(1<<TOV0)));	//  wait until t0v0=1. overflow happens
-	TIFR0|=(1<<TOV0); //WRITING 1 TO TOVO bit to make it 0 .
+		TCNT = 0;  // Initial value of TCNT0 =0
+	 	while (!(TIFR0 & (1 << TOV0)));	//  wait until t0v0=1. overflow happens
+	 	TIFR0 |= (1 << TOV0); //WRITING 1 TO TOVO bit to make it 0 .
 	}
 }
 
 int main(void)
-{
-   
-    TCNT0=0; // Initial value of TCNT0 =0
+{;	
+	 // Normal mode (WGM bits = 0)
+    TCCR0A = 0x00;
+
+    // Prescaler = 1
+    TCCR0B = (1 << CS00);
+
+    // LED output
+    DDRB |= (1 << PB5);
 	
-	// configuring Timer0 for nomral mode
-	TCCR0A &=~(1<<WGM00) ;
-	TCCROA &=~(1<<WGM01) ;
-	
-	//Configuring Timer0 for  no prescalar 
-	TCCR0B|=(1<<CS00);
-	TCCR0B&=~(1<<CS01);
-	TCCR0B&=~(1<<CS02);
-	
-	//CONFIGURING A PIN 
-DDRB |=(1<<PB5); //CONFIGURING PIN 5 AS OUTPUT
 
 	while (1)
 	{
